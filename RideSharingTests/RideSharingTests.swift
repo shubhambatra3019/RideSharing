516375380026f5978c940ded642f35c8b72136b3
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-07-21 19:53:29 -0400

    Initial Commit

---
+++ RideSharingTests/RideSharingTests.swift
@@ -0,0 +1,36 @@
+//
+//  RideSharingTests.swift
+//  RideSharingTests
+//
+//  Created by Shubham Batra on 21/07/18.
+//  Copyright © 2018 Esper. All rights reserved.
+//
+
+import XCTest
+@testable import RideSharing
+
+class RideSharingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
